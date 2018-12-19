33f749f682943c6874326fc899e0b76522190893
sw <sw@localhost.localdomain>
2018-12-19 19:19:01 +0100

    wassa

---
+++ app/src/main/java/cat/lasalle/santi/actionbarexercise/MainActivity.kt
@@ -0,0 +1,37 @@
+package cat.lasalle.santi.actionbarexercise
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(my_toolbar as Toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_action_bar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.action_favorite -> showToast("refresh")
+            R.id.action_settings -> showToast("settings")
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showToast(s: String) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show()
+    }
+}
